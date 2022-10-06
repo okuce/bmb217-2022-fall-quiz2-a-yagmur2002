367b99957f3fb2153b20facea8e97913cb4a4f62
yagmur2002 <114661586+yagmur2002@users.noreply.github.com>
2022-10-06 14:35:32 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,18 +1,30 @@
-class Otobus:
-    """Otobus bilet satis takip sinifi"""
-    
+"""yağmur bayrak 20217170058"""
+ class Otobus:
+    nereden:str=""
+    nereye:str=""
+    plaka:str=""
+    bos_koltuk:int=0
+    dolu_koltuk:int=0
+
+    def init(self,nereden,nereye,plaka,bos_koltuk,dolu_koltuk) :
+        self.nereden=nereden
+        self.nereye=nereye
+        self.plaka=plaka
+        self.bos_koltuk=bos_koltuk
+        self.dolu_koltuk=dolu_koltuk
+
 
     def bilet_sat(self):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
-        
-    
+        return self.dolu_koltuk +=1
+
+
     def bilet_iade(self):
         """Otobusteki dolu koltuk sayisini 1 azaltir"""
-        
+        return self.bos_koltuk -= 1
 
-    
+
+
     def durum_yaz(self):
         """Otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""
-        
-
-
+        print("Otobüs güzergahi:{}{},Plaka:{} ,Bos Koltuk:{},Dolu Koltuk:{} ",format(self.nereden,self.nereye,self.plaka,self.bos_koltuk,self.dolu_koltuk))
